39c84ff9a7434b5774b638425ee8a4d55a989fa6
samadessouky <s.mohamed2279@nu.edu.eg>
2023-04-25 14:34:35 -0700

    my commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; c++)
+	{
+		_putchar(str[]);
+	}
+	_putchar("\n");
+	return (0);
+}
