c2deed36c690dfa950a99beb275d7e4604d9d1bb
samadessouky <s.mohamed2279@nu.edu.eg>
2023-04-26 16:25:20 -0700

    my commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - sum od two integers
+* @x: input that will be added
+* @y: input that will be added
+* Return: sum of two integers
+*
+*/
+int add(int x, int y)
+{
+	int sum;
+	sum = x + y;
+	return (sum);
+}
