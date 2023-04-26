47d22af1fc0b43597ef3b6a0292c80c074f7864a
samadessouky <s.mohamed2279@nu.edu.eg>
2023-04-26 15:27:04 -0700

    my commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,26 +14,21 @@
 
 	for (x = 0; x < 10; x++)
 	{
+		_putchar('0');
 		for (y = 0; y < 10; y++)
 		{
 			product = x * y;
-			if (y != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			else if (product >= 10)
-			{
-				_putchar((product / 10) + 48);
-				_putchar((product % 10) + 48);
-			}
-			else if (product < 10 && y != 0)
+			_putchar(',');
+			_putchar(' ');
+			if (product <= 9)
 			{
 				_putchar(' ');
-				_putchar((product % 10) + 48);
 			}
 			else
-				_putchar((product % 10) + 48);
+			{
+				_putchar((product / 10) + '0');
+			}
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
