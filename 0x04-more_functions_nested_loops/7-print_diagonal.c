c1446cf9c14944ec71478038990376b03d35b9f5
samadessouky <s.mohamed2279@nu.edu.eg>
2023-04-27 19:12:21 -0700

    my commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,14 +11,13 @@ void print_diagonal(int n)
 	int y;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	} else
+	else
 	{
 
 		for (x = 1; x <= n; x++)
 		{
-			for (y = 0; y < n; y++)
+			for (y = 0; y < x; y++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
