17cbddb9a3c7181bed60dafd456933680be423ca
samadessouky <s.mohamed2279@nu.edu.eg>
2023-04-27 17:40:24 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - product of two integers
+* @a: input that will be added
+* @b: input that will be added
+* Return: sum of two integers
+*
+*/
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
