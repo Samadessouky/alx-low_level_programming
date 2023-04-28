7b790a05f252c755c066196a7014e6b1fe5abb9d
samadessouky <s.mohamed2279@nu.edu.eg>
2023-04-27 17:48:58 -0700

    my commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ * return: always 0
+ */
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
