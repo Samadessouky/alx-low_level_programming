6132041bcee9db0c078819183f4f17e948465b77
samadessouky <s.mohamed2279@nu.edu.eg>
2023-05-08 17:39:06 -0700

    my commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,23 @@
 #include <stdio.h>
 unsigned int _strspn(char *s, char *accept)
 {
-	int x;
-	int y;
+	unsigned int x;
+	unsigned int y;
+	unsigned int j = 0;
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; accept[y] != '\0'; y++)
+		if (s[x] != 32)
 		{
-			if (accept[y] == '\0')
-				return (x);
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					j++;
+			}
 		}
+		else
+			return (j);
+
 	}
-	return (x);
+	return (j);
 }
