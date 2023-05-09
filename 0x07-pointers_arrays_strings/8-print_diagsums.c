b836fa0eda162fcd57567e76e8d07f10a1632d87
samadessouky <s.mohamed2279@nu.edu.eg>
2023-05-08 19:49:52 -0700

    my commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,7 +16,7 @@ void print_diagsums(int *a, int size)
 	for (x = 0; x < size; x++)
 		s1 = s1 + a[x * size + x];
 	for (x = size - 1; x >= 0; x--)
-		s2 += a(x * size + (size - x - 1));
+		s2 += a[x * size + (size - x - 1)];
 	printf("%d, %d\n", s1, s2);
 }
 
